397b831e117b9ff55cd82b97a2fca0258a361927
utkarsh chahal <chahal.utkarsh@yahoo.com>
2022-10-12 12:06:40 +0530

    Add project files.

---
+++ HotelListing.API/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API
+{
+    public class WeatherForecast
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
